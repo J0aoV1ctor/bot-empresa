6b0180530c57371d1485b8206f8e6373f6ed5ba6
J0aoV1ctor <deoliveira.lopesbsb@gmail.com>
2025-05-18 00:05:37 -0300

    Esqueleto do projeto

--- codigo_selenium.py
+++ codigo_selenium.py
@@ -1,21 +1,44 @@
 from selenium import webdriver
-import time 
-# abrir o navegador
-navegador = webdriver.Chrome ()
-#acessar o site
-navegador.get("https://7lm.cvcrm.com.br/gestor/")
-#colocar o navegador em tela cheia
+import time
+#abrir o navegador 
+navegador = webdriver.Chrome()
+
+#acessar um site 
+navegador.get("https://www.hashtagtreinamentos.com")
+
+#colocar o navegador em tela cheia 
 navegador.maximize_window()
+
 #selecionar um elemento na tela 
-seu_email=navegador.find_element("class name","form-control")
-#clicar no elemento
-seu_email.click()
-# encontrar varios elementos dentro da pagina
+botao_verde = navegador.find_element("class name","botao-verde")
 
-campos=navegador.find_elements("class name","form-control")
+#clicar no elemento 
+botao_verde.click() 
 
-for senha in campos:
-    if senha in campos.text:
-        seu_email.click()
+#selecionar mais de um elemento 
+lista_botoes = navegador.find_elements("class name","header__titulo")
+for botao in lista_botoes:
+    if "Assinatura" in botao.text:
+        botao.click()
+        break
+#selecionar uma aba
+abas = navegador.window_handles
+navegador.switch_to.window(abas[1])
+
+# navegar para um site diferete
+navegador.get("https://www.hashtagtreinamentos.com/curso-python")
+
+# escrever em um campo/formulario 
+navegador.find_element("id","firstname").send_keys("João Victor")
+time.sleep(1)
+navegador.find_element("id","email").send_keys("lalala@gmail.com ")
+time.sleep(1)
+navegador.find_element("id","phone").send_keys("(61)992537592")
+time.sleep(1)
+
+botao_quero_clicar = navegador.find_element("id","_form_2475_submit")
+
+#dar scroll (colocar um elemento na tela)
+navegador.execute_script("arguments[0].scrollIntoView({block: 'center'})", botao_quero_clicar )
 
 time.sleep(10)
